4bc895a52aae91bd6509c86a0f49d07468f7a3cd
Daniel Washington <danielvertigo@hotmail.com>
2021-06-13 16:15:27 -0300

    Initial Commit

---
+++ Longest Sequence of Consecutive Zeroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Longest Sequence of Consecutive Zeroes
+//
+//  Created by Daniel Washington Ignacio on 13/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
